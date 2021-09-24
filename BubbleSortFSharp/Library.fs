44732fa0707eb7aff88d957f0f0332072faf412c
v.yakovlev <wwevovancik@gmail.com>
2021-09-24 18:39:04 +0300

    Bubble Sort

---
+++ BubbleSortFSharp/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace BubbleSortFSharp
+
+module BubbleSortF =
+    let rec sort (a: int []) =
+      let mutable fin = true
+      for i in 0..a.Length-2 do
+        if a.[i] > a.[i+1] then
+          let t = a.[i]
+          a.[i] <- a.[i+1]
+          a.[i+1] <- t
+          fin <- false
+      if not fin then sort a
---
+++ BubbleSortFromFSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BubbleSort;
+
+namespace BubbleSortFromFSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[10];
+            Random r = new Random();
+
+            Console.WriteLine("Unsorted Array");
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = r.Next(0, 100);
+                Console.WriteLine(Arr[i]);
+            }
+            int[] SortArr = new int[10];
+            SortArr = Bubble.sort(Arr);
+            Console.WriteLine("Sorted Array");
+            for (int i = 0; i < SortArr.Length; i++)
+            {
+                Console.WriteLine(SortArr[i]);
+            }
+        }
+    }
+}
